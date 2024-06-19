21ce697705a66e79a16fb06b0217389d1488465e
jude7733 <judeputhussery360@gmail.com>
2024-06-19 20:26:40 +0530

    FormComponent created and reused
    edited parent layout
    changed name to rollNo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,5 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Terminal } from "lucide-react";
-import { AlertCircle } from "lucide-react";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   Card,
   CardContent,
@@ -15,85 +12,58 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+type FormComponentProps = {
+  mode: "student" | "institution";
+};
+
+const FormComponent = ({ mode }: FormComponentProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{mode} account</CardTitle>
+        <CardDescription>Login here</CardDescription>
+      </CardHeader>
+      <form>
+        <CardContent className="space-y-2">
+          {mode === "student" ? (
+            <div className="space-y-1">
+              <Label htmlFor="rollNo">Roll Number</Label>
+              <Input id="rollNo" required />
+            </div>
+          ) : (
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" required />
+            </div>
+          )}
+          <div className="space-y-1">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Link href={mode === "student" ? "/dashboard" : "/lookup"}>
+            <Button type="submit">Login</Button>
+          </Link>
+        </CardFooter>
+      </form>
+    </Card>
+  );
+};
+
 export default function Login() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Student</TabsTrigger>
-          <TabsTrigger value="password">Institution</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>Login here.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" />
-              </div>
-
-              <div className="space-y-1">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
-              </div>
-            </CardContent>
-
-            <CardFooter>
-              <Link href="/dashboard">
-                <Button>Login</Button>
-              </Link>
-            </CardFooter>
-          </Card>
-          <Alert>
-            <Terminal className="h-4 w-4" />
-            <AlertTitle>Success</AlertTitle>
-            <AlertDescription>Login in progress</AlertDescription>
-          </Alert>
-
-          <Alert variant="destructive">
-            <AlertCircle className="h-4 w-4" />
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>Wrong Username or Password.</AlertDescription>
-          </Alert>
-        </TabsContent>
-
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Institution Account</CardTitle>
-              <CardDescription>Login here</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Link href="/lookup">
-                <Button>Login</Button>
-              </Link>
-            </CardFooter>
-          </Card>
-          <Alert>
-            <Terminal className="h-4 w-4" />
-            <AlertTitle>Success</AlertTitle>
-            <AlertDescription>Login in progress</AlertDescription>
-          </Alert>
-          <Alert variant="destructive">
-            <AlertCircle className="h-4 w-4" />
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>Wrong Username or Password.</AlertDescription>
-          </Alert>
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue="student" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="student">Student</TabsTrigger>
+        <TabsTrigger value="institution">Institution</TabsTrigger>
+      </TabsList>
+      <TabsContent value="student">
+        <FormComponent mode="student" />
+      </TabsContent>
+      <TabsContent value="institution">
+        <FormComponent mode="institution" />
+      </TabsContent>
+    </Tabs>
   );
 }
