721d56eca2ede6f74e6363cfa813463bae6de655
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2024-06-25 00:33:24 +0530

    created base for dashboard

--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -1,11 +1,59 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function ButtonDemo() {
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+export default function ResizableDemo() {
   return (
-    <Link href="/student/EditProfile">
-      <Button>Edit Profile</Button>
-    </Link>
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-screen max-w-full rounded-lg border"
+    >
+      <ResizablePanel defaultSize={25}>
+        <div className="flex h-full items-center justify-center p-6">
+          <Link href="/student/EditProfile">
+            <Button>Edit Profile</Button>
+          </Link>
+        </div>
+      </ResizablePanel>
+      
+      <ResizableHandle withHandle />
+
+  
+      <ResizablePanel defaultSize={50}>
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel defaultSize={25}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Certificates</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={25}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Projects</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={50}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Notice Board</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
+      
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={25}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Notification</span>
+        </div>
+      </ResizablePanel>
+  
+
+    </ResizablePanelGroup>
   );
 }
-
