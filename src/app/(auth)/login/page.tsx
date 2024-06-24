0cd2bf50e3cf193ad3b9cdb46dddff494cb987c5
jude7733 <judeputhussery360@gmail.com>
2024-06-24 20:44:12 +0530

    grouped to auth, fixed login page issues
    cleaned code
    replaced raw icons to packages for lucide
    fixed Image src as png should be public file

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,73 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { UserAuthForm } from "./user-auth-form";
+import { Command } from "lucide-react";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <Command size={24} className="mr-2" />
+          Teconiq-dev
+        </div>
+        <div className="relative z-20 mt-auto">
+          <div className="w-full flex flex-col justify-center">
+            <Image
+              src="/dark_auth.png"
+              width={420}
+              height={420}
+              alt="Authentication"
+              className="hidden lg:block"
+            />
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Students Login
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to login
+            </p>
+          </div>
+          <UserAuthForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
