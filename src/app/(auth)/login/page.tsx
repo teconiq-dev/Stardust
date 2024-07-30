3266fee51d5aaa3248f52a877b15458952aaa253
jude7733 <judeputhussery360@gmail.com>
2024-07-31 02:32:09 +0530

    Login animated tabs and responsiveness
    fix bg
    wrap reverse icon-cloud section in phone

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,103 +1,32 @@
-"use client";
-import Link from "next/link";
 import { UserAuthForm } from "./user-auth-form";
-import { useTheme } from "next-themes";
-
-import {
-  ToggleGroup,
-  ToggleGroupItem,
-} from "@/components/ui/toggle-group"
-import React, { useState } from "react";
-import Studentsvg from "../../../components/Icons/studentsvg";
-import OrgSvg from "../../../components/Icons/organizationsvg";
-import TeachSvg from "../../../components/Icons/teacher";
-import Terms from "@/app/legal/page";
+import { Tabs } from "@/components/ui/tabs";
 
 export default function AuthenticationPage() {
-  const { theme } = useTheme();
-  const [selectedRole, setSelectedRole] = useState("Student");
-  const [, setReset] = useState(false);
+  const tabs = [
+    {
+      title: "Student",
+      value: "student",
+      content: <UserAuthForm selectedRole="Student" />,
+    },
+    {
+      title: "Organization",
+      value: "organization",
+      content: <UserAuthForm selectedRole="Organization" />,
+    },
+    {
+      title: "Teacher",
+      value: "teacher",
+      content: <UserAuthForm selectedRole="Teacher" />,
+    },
+  ];
   return (
-    <div className="mx-auto flex flex-1 w-full flex-col justify-center space-y-6 bg-transparent">
-      {
-        // light and dark custom background
-        theme === "light" ? (
-          <div className="absolute inset-0 z-[-2] h-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
-          </div>
-        ) : (
-          <div className="absolute top-0 inset-0 z-[0] h-full bg-purple-950/10 bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        )
-      }
-
-      <div
-          className="flex flex-col m-14 p-10 space-y-2 text-center transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]">
-
-        <div>
-          <h1 className="text-2xl font-semibold tracking-tight transition-all duration-300">
-            {selectedRole} Login
-          </h1>
-          <p className="text-sm text-muted-foreground transition-all duration-300">
-            Enter your {selectedRole}ID below to login
-          </p>
-        </div>
-        <div>
-          <div >
-            <ToggleGroup size={"lg"}  type="single"  onValueChange={setSelectedRole} defaultValue="Student">
-              <ToggleGroupItem value="Student" aria-label="Toggle bold"
-                onMouseEnter={() => setReset(false)}
-                onMouseLeave={() => setReset(true)}
-                className="px-8 margin-8 group flex transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]  border-white/10  items-center justify-center py-8 border rounded-lg hover:bg-transparent/50 duration-150 active:bg-transparent/50"
-            >
-              <Studentsvg/>
-              <span className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                Student
-              </span>
-              </ToggleGroupItem>
-              <ToggleGroupItem value="Organization" aria-label="Toggle bold"
-                onMouseEnter={() => setReset(false)}
-                onMouseLeave={() => setReset(true)}
-                className="px-8 mx-5 group flex transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]  border-white/10  items-center justify-center py-8 border rounded-lg hover:bg-transparent/50 duration-150 active:bg-transparent/50"
-            >
-              <OrgSvg/>
-              <span className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                Organization
-              </span>
-              </ToggleGroupItem>
-              <ToggleGroupItem value="Faculty" aria-label="Toggle bold"
-                onMouseEnter={() => setReset(false)}
-                onMouseLeave={() => setReset(true)}
-                className="px-8 group flex transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]  border-white/10  items-center justify-center py-8 border rounded-lg hover:bg-transparent/50 duration-150 active:bg-transparent/50"
-            >
-              <TeachSvg/>
-              <span className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                Teacher
-              </span>
-              </ToggleGroupItem>
-            </ToggleGroup>
-            {/*TODO: during hover make icon invisible or give background to text */}
-          </div>
-        </div>
-        <UserAuthForm selectedRole={selectedRole}/>
-        <p className="px-8 text-center text-sm text-muted-foreground">
-        By clicking continue, you agree to our{" "}
-          <Link
-              href="/src/app/legal/page"
-              className="underline underline-offset-4 hover:text-primary"
-          >
-            Terms of Service
-            {/*TODO: add terms of service page(already created)*/}
-          </Link>{" "}
-          and{" "}
-          <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-          >
-            Privacy Policy
-            {/*TODO: add privacy policy page(already created)*/}
-          </Link>
-          .
-        </p>
+    <div className="flex flex-1 w-full flex-col justify-center bg-transparent p-5">
+      <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative flex flex-col max-w-5xl mx-auto w-full items-start justify-start mb-4">
+        <Tabs
+          tabs={tabs}
+          contentClassName="backdrop-blur-lg h-fit bg-gradient-to-r from-blue-500/5 to-purple-500/5"
+          containerClassName="flex justify-center mt-10"
+        />
       </div>
     </div>
   );
--- src/app/(auth)/login/user-auth-form.tsx
+++ src/app/(auth)/login/user-auth-form.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import * as React from "react";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { LoaderCircle } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
+interface UserAuthFormProps {
   selectedRole: string;
 }
 
-export function UserAuthForm({ className, selectedRole, ...props }: UserAuthFormProps) {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+export function UserAuthForm({ selectedRole }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
@@ -25,62 +23,69 @@ export function UserAuthForm({ className, selectedRole, ...props }: UserAuthForm
   }
 
   return (
-    <div>
-      <div className={cn("grid gap-6", className)} {...props}>
-        <form onSubmit={onSubmit}>
-          <div className="grid gap-2">
-            <div className="grid gap-1">
-              <Label className="sr-only" htmlFor="userID">
-                Student ID
-              </Label>
-              <Input
-                id="userID"
-                placeholder="ID"
-                type="text"
-                autoCapitalize="none"
-                autoComplete="username"
-                autoCorrect="off"
-                disabled={isLoading}
-              />
-            </div>
-            <div className="grid gap-1">
-              <Label className="sr-only" htmlFor="password">
-                Password
-              </Label>
-              <Input
-                id="password"
-                placeholder="Password"
-                type="password"
-                autoCapitalize="none"
-                autoComplete="current-password"
-                autoCorrect="off"
-                disabled={isLoading}
-              />
-            </div>
+    <div className="relative overflow-hidden rounded-xl flex flex-col p-10 gap-3 text-center transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]">
+      <h1 className="text-2xl font-semibold tracking-tight transition-all duration-300">
+        {selectedRole} Login
+      </h1>
+      <p className="text-sm text-muted-foreground transition-all duration-300">
+        Enter your {selectedRole} ID below to login
+      </p>
+      <form onSubmit={onSubmit}>
+        <div className="flex gap-3 flex-col items-center justify-center">
+          <Input
+            id="userID"
+            placeholder="ID"
+            type="text"
+            autoCapitalize="none"
+            autoComplete="username"
+            autoCorrect="off"
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            placeholder="Password"
+            type="password"
+            autoCapitalize="none"
+            autoComplete="current-password"
+            autoCorrect="off"
+            disabled={isLoading}
+          />
+          <Link
+            href={
+              selectedRole === "Student"
+                ? "/student/dashboard"
+                : selectedRole === "Organization"
+                  ? "/organization/lookup"
+                  : "/teacher/dashboard"
+            }
+          >
+            <Button disabled={isLoading}>
+              {isLoading && (
+                <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Sign In
+            </Button>
+          </Link>
+        </div>
+      </form>
 
-            {selectedRole === "Student" && (
-                <Link href="/student/dashboard" className="grid gap-2">
-                  <Button disabled={isLoading}>
-                    {isLoading && (
-                        <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
-                    )}
-                    Sign In
-                  </Button>
-                </Link>
-            )}
-            {selectedRole === "Organization" && (
-                <Link href="organization/lookup" className="grid gap-2">
-                  <Button disabled={isLoading}>
-                    {isLoading && (
-                        <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
-                    )}
-                    Lookup Sign In
-                  </Button>
-                </Link>
-            )}
-          </div>
-        </form>
-      </div>
+      <footer className="px-8 text-center text-sm text-muted-foreground">
+        By clicking continue, you agree to our{" "}
+        <Link
+          href="/terms"
+          className="underline underline-offset-4 hover:text-primary"
+        >
+          Terms of Service
+        </Link>{" "}
+        and{" "}
+        <Link
+          href="/privacy"
+          className="underline underline-offset-4 hover:text-primary"
+        >
+          Privacy Policy
+        </Link>
+        .
+      </footer>
     </div>
   );
 }
